17e4358fe013c05fed530af58cb4bd96225d79fb
Kushal Pisavadia <kushi.p@gmail.com>
2016-08-31 14:55:36 +0100

    Use #data_source_exists? over #table_exists? to prevent deprecation warning
    This prevents the following warning from appearing:
    
    ```
    DEPRECATION WARNING: table_exists? is deprecated and will be removed
    from Rails 5.1 (use #data_source_exists? instead)
    ```

--- lib/postgres_ext/active_record/relation.rb
+++ lib/postgres_ext/active_record/relation.rb
@@ -1,10 +1,15 @@
-gdep = Gem::Dependency.new('activerecord', '>= 4.2.0')
-ar_version_cutoff = gdep.matching_specs.sort_by(&:version).last
+gdep_4_2 = Gem::Dependency.new('activerecord', '>= 4.2.0')
+ar_4_2_version_cutoff = gdep_4_2.matching_specs.sort_by(&:version).last
+
+gdep_5_0 = Gem::Dependency.new('activerecord', '~> 5.0.0')
+ar_5_0_version_cutoff = gdep_5_0.matching_specs.sort_by(&:version).last
 
 require 'postgres_ext/active_record/relation/merger'
 require 'postgres_ext/active_record/relation/query_methods'
 
-if ar_version_cutoff
+if ar_5_0_version_cutoff
+  require 'postgres_ext/active_record/5.0/relation/predicate_builder/array_handler'
+elsif ar_4_2_version_cutuff
   require 'postgres_ext/active_record/relation/predicate_builder/array_handler'
 else
   require 'postgres_ext/active_record/4.x/relation/predicate_builder'
